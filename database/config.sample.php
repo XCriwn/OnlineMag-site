7406c1f22dac924f41d354137aefb57a5c933d59
Cosmin Achim <office@akimonline.com>
2024-08-20 14:08:46 +0300

    Remove real config file from git project and add sample config file

---
+++ database/config.sample.php
@@ -0,0 +1,12 @@
+<?php
+$config = [
+    'database' => [
+    'host'       => "localhost",
+    'port'       => "3306",
+    'dbname'     => "[YOUR_DATABASE_NAME]",
+    'dbpasswprd' => "[YOUR_DATABASE_PASSWORD]",
+    'charset'    => "utf8mb4",
+    ],
+];
+
+return $config;
