96622885d1d6967d2051c0eaff757cf9b8e34582
Cosmin Achim <office@akimonline.com>
2024-08-20 15:39:09 +0300

    Code review --wip

--- core/Authenticator.php
+++ core/Authenticator.php
@@ -6,37 +6,59 @@ use database\Database;
 
 class Authenticator
 {
-
-    public function attempt($email, $password){
+    /**
+     * Attempt to login the user with the given email and password
+     *
+     * @param $email
+     * @param $password
+     * @return mixed
+     */
+    public function attempt($email, $password) : mixed
+    {
         $user = App::resolve(Database::class)->query('select * from users where email = :email', [
             'email' => $email
         ])->find();
 
-        if ($user) {
-            if (password_verify($password, $user['password'])) {
-                $this->login([
-                    'email' => $email,
-                    'role' => $user['role'],
-                    'id' => $user['id']
-                ]);
-                return $user;
-            }
+        if (!$user) {
+            return false;
         }
 
-        return false;
+        if (!password_verify($password, $user['password'])) {
+            return false;
+        }
+
+        $this->login([
+            'id'    => $user['id'],
+            'email' => $email,
+            'role'  => $user['role'],
+        ]);
+
+        return $user;
     }
 
-    public static function login($user){
+    /**
+     * Add logged in user detail to session
+     *
+     * @param $user
+     */
+    public static function login($user): void
+    {
         $_SESSION['user'] = [
+            'id'    => $user['id'],
             'email' => $user['email'],
-            'role' => $user['role'],
-            'id' => $user['id'],
+            'role'  => $user['role'],
         ];
 
         session_regenerate_id(true);
     }
 
-    public static function logout(){
+    /**
+     * Log out the user and destroy the session
+     *
+     * @return void
+     */
+    public static function logout(): void
+    {
         Session::destroy();
     }
 }
--- views/products/create.view.php
+++ views/products/create.view.php
@@ -42,13 +42,13 @@
                                     </div>
                                     <label for="price" class="block text-sm font-medium leading-6 text-gray-900">Price:</label>
                                     <div class="mt-2">
-                                        <textarea
+                                        <input
                                             id="price"
                                             name="price"
-                                            rows="1"
+                                            type="number"
                                             placeholder="Write here the price of the item..."
                                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                        ><?= $_POST['price'] ?? ''?></textarea>
+                                        ><?= $_POST['price'] ?? ''?></input>
 
                                         <?php if(isset($errors['price'])) : ?>
                                             <p class="text-red-500 text-xs mt-2"><?= $errors['price']?></p>
